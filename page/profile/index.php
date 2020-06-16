0cf15365debb09137697a0ee60501f776e3da706
Greg Bowler <greg.bowler@g105b.com>
2020-06-16 21:22:10 +0100

    Profile page

--- page/profile/index.php
+++ page/profile/index.php
@@ -1,18 +1,15 @@
 <?php
 namespace Authwave\Page\Profile;
 
+use Authwave\Application\ApplicationDeployment;
 use Authwave\User\User;
 use Gt\WebEngine\Logic\Page;
 
 class IndexPage extends Page {
 	public User $user;
+	public ApplicationDeployment $deployment;
 
 	public function go():void {
-		if(isset($this->user)) {
-			$this->document->getElementById("output")->innerText = "Logged in to client as " . $this->user->getEmail();
-		}
-		else {
-			$this->document->getElementById("output")->innerText = "Not logged in to client";
-		}
+
 	}
 }
